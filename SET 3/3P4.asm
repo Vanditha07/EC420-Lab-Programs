275dcbc686ccb817afd856a5c7ba878e732101c0
Vanditha07 <vandithamogra@gmail.com>
2021-05-25 05:40:29 +0530

    sixth commit

--- SET 3/3P4.asm
+++ SET 3/3P4.asm
@@ -1,29 +1,23 @@
 //Read the data from Port 1. If P1.1 is at logic 0, find the largest number in an array of ‘N’ bytes of data and store in location 40h. 
 //If P1.0 is at logic 1, find the smallest number in the array and store in the location 40h.  
 
-		   org 00h
-		   mov r1, #0ah      ;counter
-		   mov r0, #30h    ;source pointer
-		   mov a, #00h     ;initial min/max is zero
-		   mov p1, #0ffh   ;configure p1 as input port
-		   jb p1.0, smallest
-		   clr c
-repeat1:   subb a, @r0
-		   jnc noexch
-	       mov a, @r0
-noexch:    inc r0
-	       djnz r1, repeat1
-	       mov 40h, a
-	       sjmp last	
-smallest:  clr c
-repeat2:   subb a, @r0
-	       jc noexch2
-	       mov a, @r0
-noexch2:   inc r0
-	       djnz r1, repeat2
-	       mov 40h, a
-last:      mov b, a
-	       end
+		org 00h
+		mov r0, #30h
+		mov r1, #05h
+		dec r1
+		mov a, @r0
+		inc r0
+repeat: mov b, @r0
+		cjne a, b, next
+next: 	mov b, p1
+		jb b.1, next1
+		jnc again
+		sjmp again1
+next1:  jc again
+again1: mov a, @r0
+again:  inc r0
+		djnz r1, repeat
+		mov 40h, a
+		end
 		
-	
 	
--- SET 4/4P5.asm
+++ SET 4/4P5.asm
@@ -1,1 +1,17 @@
 // program for BCD up counter. Show each count in Port 0 with appropriate delay
+	    org 00h
+	    mov a, #00h
+	    mov r3, #0ffh
+repeat: mov p0, a
+	    acall delay
+		add a, #01h
+		da a
+		djnz r3, repeat
+delay:  mov r0, #0ffh
+repeat2:mov r1, #0ffh
+repeat1:djnz r1, repeat1
+		djnz r0, repeat2
+		ret 
+	
+		end
+		
